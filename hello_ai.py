430c551ec930639e47d215bad331b27ebeb306ea
Rahul Sinha <huntrahul@gmail.com>
2026-06-02 16:35:13 -0400

    corrected env

--- hello_ai.py
+++ hello_ai.py
@@ -4,8 +4,11 @@ from openai import OpenAI
 load_dotenv()
 client = OpenAI()
 
-r = client.chat.completions.create(
-    model    = 'gpt-4o-mini',
-    messages = [{'role': 'user', 'content': 'Say hello!'}]
+response = client.chat.completions.create(
+    model="gpt-4o-mini", 
+    messages=[
+        {"role": "user", "content": "Hello!"}
+    ]
 )
-print(r.choices[0].message.content)     # Hello! How can I help you?
+
+print(response.choices[0].message.content)
